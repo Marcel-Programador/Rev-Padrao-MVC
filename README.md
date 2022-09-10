8dfd5b9620e4e3ec837da7c2148e7c16fd96a595
Marcel-Programador <backing.audio@gmail.com>
2022-09-10 17:16:13 -0300

    Revisão da aula do dia 02062022 completa

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Rev-Padrao-MVC
+
+# Fluxo da estrutura de pastas
+*View Requisita ou Envia algo.
+*app.js verifica se nele existe alguma rota para aquela solicitação.
+Existindo ele entra na rota.
+*Entra na pasta "routes" e acessa a rota.
+Verifica se aquela rota esta configuradan econtra o caminho configurado no Controller.
+*O Controller identifica a solicitação e responde para a View finalizando o fluxo.
+
+# Responsabilidade de cada pasta dentro do MVC
+Views:
+Area de interação com usuario final(Navegador)
+
+Controllers:
+Recebe as requisições da View interpreta e envia para Route que envia a solicitação para model.
+
+Routers:
+Rotas caminho para encontrar a informação requisitada pela View
+
+Models
+Devolve ao Controller a resposta solicitada
--- app.js
+++ app.js
@@ -4,35 +4,10 @@ const app = express();
 
 const port = 3000;
 
+const userRoute = require("./src/routes/userRoute")
 
-// Rota para teste retornando "Hello World".
-// app é a variavel utilizada para utilizar algum recurso do pacote express neste caso o metdodo "get".
-// Que realizara uma consulta(req) e retornara(res) a "/" no inicio da estrutura representa a pagina
-// raiz "no caso localhost:3000/".
-// Ainda sobre esta estrutura o parametro "res" utilizando o metodo "send" ficando "res.send" retornara.
-// para o usuario do view a resposta "Hello World".
-app.get("/", (req, res) => {
-    res.send("Hello World - Home page");
-});
-
-// O metodo post retorna uma resposta diferente do GET.
-// O GET retorna uma resposta seja VIEW ou um OBJETO, ja o POST retorna um OBJETO.
-app.post("/", (req, res) => {
-    res.send("Hello World - Post");
-});
-
-app.get("/product", (req, res) => {
-    // res.send neste caso poderia esta retornando o caminho daq pagina.
-    // Exemplo res.send("/product, routeProduct")
-    res.send("Bem vindo a pagina produtos");
-});
-
-app.get("*", (req, res) => {
-    res.send("404 - Not Found");
-});
-// Outro recurso do express "listem".
-// Onde o retorno estamos utilizando para verificar que estamos rodando na porta 3000
+app.use(userRoute);
 
 app.listen(port, () => {
-    console.log("Estou rodando na porta: " + port);
+    console.log("Estamos rodando na porta: " + port);
 });
